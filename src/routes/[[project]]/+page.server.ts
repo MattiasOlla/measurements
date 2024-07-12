8b0da11d240a62b96e13148b1c79b9772b59195d
Mattias <mattias.olla@gmail.com>
2024-07-12 19:31:04 +0200

    Fix redirect

--- src/routes/[[project]]/+page.server.ts
+++ src/routes/[[project]]/+page.server.ts
@@ -1,19 +1,19 @@
-import { goto } from "$app/navigation";
 import * as db from "$lib/db";
+import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params, locals }) => {
   const session = await locals.auth();
   if (!session) {
-    if (params.project) goto("/");
+    if (params.project) redirect(307, "/");
     return { projects: [] };
   }
 
   await db.upsertUser(session.user.id);
   const projects = await db.getUserProjects(session.user.id);
-  if (params.project) {
-    const activeProject = projects.find((project) => project.slug === params.project);
-    if (activeProject) return { projects, activeProject };
-  }
-  return { projects };
+  if (!params.project) return { projects };
+
+  const activeProject = projects.find((project) => project.slug === params.project);
+  if (!activeProject) redirect(307, "/");
+  return { projects, activeProject };
 };
