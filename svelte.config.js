a50e7efec1eac736976eb2e222f89d61b11c68c7
Mattias <mattias.olla@gmail.com>
2024-07-16 21:02:08 +0200

    Remove sass
    It's just clunky and gets in the way

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,9 @@
 import adapter from "@sveltejs/adapter-node";
-import { sveltePreprocess } from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
-  },
-  preprocess: sveltePreprocess({
-    scss: {
-      prependData: "@import './src/app.scss';",
-    },
-  }),
-  onwarn: (warning, handler) => {
-    const { code, frame } = warning;
-    if (code === "css-unused-selector") return;
-    handler(warning);
   },
 };
 
