ed53a70736b8cb80d37d2059453dd6b67e271732
Mattias <mattias.olla@gmail.com>
2024-07-26 21:26:10 +0200

    Add debug route
    This will just JSON stringify all the user's projects

---
+++ src/routes/debug/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  const { data } = $props();
+  const { projects } = data;
+</script>
+
+<section class="section">
+  <pre>
+    {JSON.stringify({ projects }, null, 2)}
+  </pre>
+</section>
