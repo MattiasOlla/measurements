a1349a721491c7a0cf1449484caca8d64938c20e
Mattias <mattias.olla@gmail.com>
2024-08-26 11:22:55 +0200

    Redirect to root if user is not logged in

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,13 @@
 import * as db from "$lib/db";
+import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async (event) => {
-  const session = await event.locals.auth();
-
-  if (!session?.user?.id) return { session };
+export const load: LayoutServerLoad = async ({ locals, params, request, url }) => {
+  const session = await locals.auth();
+  if (!session?.user?.id) {
+    if (url.pathname !== "/") redirect(307, "/");
+    return { session };
+  }
 
   await db.upsertUser(session.user.id);
   const projects = await db.getUserProjects(session.user.id);
